60dec9bea8611c26f719b25a45a6b2122408d564
bharath <karla.bharatrhnandan@gmail.com>
2021-03-25 12:59:16 +0530

    added

--- api/routes/queries.js
+++ api/routes/queries.js
@@ -1,10 +1,10 @@
-const Standup = require('../../models/books')
+const Book = require('../../models/books')
 
 module.exports = function (router) {
 
     // POST: Get meeting note document...
     router.post('/book', function (req, res) {
-        let note = new Standup(req.body)
+        let note = new Book(req.body)
         note.save(function (err,note) {
             if (err) {
                 return res.status(400).json(err)
@@ -18,7 +18,7 @@ module.exports = function (router) {
     })
 
     router.get('/popular',(req,res)=>{
-        Standup.find().collation({'locale':'en'}).sort({rating:-1}).then((err,result)=>{
+        Book.find().collation({'locale':'en'}).sort({rating:-1}).then((err,result)=>{
             if (err) return res.status(400).json(err)
             if (s==null) {
                 return res.status(400).json(err)
@@ -28,7 +28,7 @@ module.exports = function (router) {
     })
 
     router.get('/new',(req,res)=>{
-        Standup.find().collation({'locale':'en'}).sort({year:-1}).then((err,result)=>{
+        Book.find().collation({'locale':'en'}).sort({year:-1}).then((err,result)=>{
             if (err) return res.status(400).json(err)
             if (s==null) {
                 return res.status(400).json(err)
@@ -37,7 +37,7 @@ module.exports = function (router) {
         });
     })
     router.get('/collection',(req,res)=>{
-        Standup.find().collation({'locale':'en'}).then((err,result)=>{
+        Book.find().collation({'locale':'en'}).then((err,result)=>{
             if (err) return res.status(400).json(err)
             if (s==null) {
                 return res.status(400).json(err)
@@ -46,7 +46,7 @@ module.exports = function (router) {
         });
     })
     router.get('/authors',(req,res)=>{
-        Standup.find().collation({'locale':'en'}).sort({"author":1}).then((err,result)=>{
+        Book.find().collation({'locale':'en'}).sort({"author":1}).then((err,result)=>{
             if (err) return res.status(400).json(err)
             if (s==null) {
                 return res.status(400).json(err)
@@ -56,7 +56,7 @@ module.exports = function (router) {
     })  
     
     router.get("/search/:id",(req,res)=>{
-        Standup.find(
+        Book.find(
             {$or:[
                 {"author":{'$regex': req.params.id,$options:'i'}},
                 {"title":{'$regex': req.params.id,$options:'i'}},
